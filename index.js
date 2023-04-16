fd9a48bc43548d8b55a0cca3736a9c22ba97af53
jorge fernandez <max1-29@hotmail.com>
2023-04-16 12:54:07 -0300

    se agrega carrito

--- index.js
+++ index.js
@@ -4,24 +4,23 @@ const inputApellido = document.getElementById("apellido")
 const titulo = document.getElementById("titulo")
 
 //click sobre el boton ingresar
-formulario.onsubmit = (e)=>{
+formulario.onsubmit = (e) => {
   e.preventDefault()
   const infoUsuario = {
-      nombre:inputNombre.value,
-      apellido: inputApellido.value
+    nombre: inputNombre.value,
+    apellido: inputApellido.value
   }
   localStorage.setItem("infoUsuario", JSON.stringify(infoUsuario))
-  formulario.remove()
+
   titulo.innerText = `BIENVENIDO ${infoUsuario.nombre} ${infoUsuario.apellido}`
 }
 
-const infoUsuario = JSON.parse (localStorage.getItem("infoUsuario"))
-if(infoUsuario){
-  formulario.remove()
+const infoUsuario = JSON.parse(localStorage.getItem("infoUsuario"))
+if (infoUsuario) {
+
   titulo.innerText = `BIENVENIDO ${infoUsuario.nombre} ${infoUsuario.apellido}`
 
 }
-
 
 
 
@@ -31,35 +30,59 @@ if(infoUsuario){
 
 
 //recuperar elementos del DOM
-const divProductos =document.getElementById("productos")
-const botonFinalizar = document.getElementById("finalizar")
+const divProducts = document.getElementById("productos")
+const finishButton = document.getElementById("finalizar")
 //funcion que ejecute el fetch
-const fetchProducts = async()=>{
-  const productsApi =await fetch('https://fakestoreapi.com/products')
-  const productsJSON =await productsApi.json()
+const fetchProducts = async () => {
+  const productsApi = await fetch('https://fakestoreapi.com/products')
+  const productsJSON = await productsApi.json()
   //console.log(productsJSON);
   return productsJSON
 }
 
-
+//FUNCION QUE EJECUTE EL FETCH DE UN SOLO PRODUCTO
+const fetchOneProductroduct = async (id) => {
+  const productApi = await fetch(`https://fakestoreapi.com/products${id}`)
+  const productJSON = await productApi.json()
+  //console.log(productJSON)
+  return productJSON
+}
 // FUNCION PRA RENDERIZAR LOS PRODUCTOS
 
- const renderproducts = async()=>{
+const renderproducts = async () => {
   const products = await fetchProducts()
-  products.forEach((prod)=>{
-    const{id, title, price, category, image} = prod
-    divProductos.innerHTML+=`
+  products.forEach((prod) => {
+    const { id, title, price, category, image } = prod
+    divProducts.innerHTML += `
     <div class="card cardProducto">
   <img src="${image}" alt="...">
   <div class="card-body">
     <h5 class="card-title">${title}</h5>
     <p class="card-text">${price} ${category}</p>
-    <button id=${id}>AGREGAR</button>
+    <button id=${id} onclick="addProduct(${id})">AGREGAR</button>
   </div>
 </div>
     `
   }
   )
- }
+}
 renderproducts()
-   
+
+//colocar producto en carrito
+const cart = []
+
+const addProduct = async (id) => {
+  const product = await fetchOneProductroduct(id)
+  const searchProductCart = cart.find(prod => prod.id === product.id)
+  if (!searchProductCart) {
+    cart.push({
+      id: product.id,
+      name:product.title,
+      quantify: 1,
+      price: product.price
+    })
+  } else {
+    searchProductCart.quantify++
+  }
+  console.log(cart);
+}
